f10d1c473bcd3a6d695602ed8f79bf1fb1c3567c
Morten Sørvig <morten.sorvig@qt.io>
2023-03-07 00:26:04 +0100

    Use "early return"-style main()

--- app/main.cpp
+++ app/main.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-    Application app(argc, argv, QStringLiteral("Slate"));
-    if (app.qmlEngine()->rootObjects().isEmpty())
+    Application *app = new Application(argc, argv, QStringLiteral("Slate"));
+    if (app->qmlEngine()->rootObjects().isEmpty())
         return 1;
-    return app.run();
+    return 0;
 }
